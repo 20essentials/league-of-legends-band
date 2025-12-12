5ce61651f3c75e1b0ec53ba1e4a0809e1953bd0d
DominicDev <s0l0estudi4r@gmail.com>
2025-12-12 11:08:34 +0100

    v0.0.2

---
+++ config.js
@@ -0,0 +1,23 @@
+export const nameBand = ' LEAGUE OF LEGENDS';
+export let nameSongs = [
+	"AWAKEN ", /*1*/
+	"WARRIORS", /*2*/
+	"WARSONGS", /*3*/
+	"PROJECT: YI", /*4*/
+	"LEGENDS NEVER DIE ", /*5*/
+	"PHOENIX ", /*6*/
+	"GODS ", /*7*/
+	"BURN IT ALL DOWN", /*8*/
+	"GET JINXED", /*9*/
+	"IGNITE ", /*10*/
+	"RISE ", /*11*/
+	"TAKE OVER ", /*12*/
+	"STAR WALKING", /*13*/
+	"WORLDS COLLIDE", /*14*/
+	"EDGE OF INFINITY", /*15*/
+	"KIN OF THE STAINED BLADE", /*16*/
+	"YONE, THE UNFORGOTTEN", /*17*/
+	"APHELIOS, THE WEAPON OF THE FAITHFUL", /*18*/
+	"GWEN, THE HALLOWED SEAMSTRESS", /*19*/
+	"VIEGO, THE RUINED KING", /*20*/
+]
